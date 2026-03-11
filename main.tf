ddc806d19cc43cb5e261e08524299768f0bdf58d
ankushmohite <131748560+ankushmohite@users.noreply.github.com>
2026-03-11 16:33:56 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "new_bucket" {
+  bucket = "shubhamdhole972026"
+
+  tags = {
+    Name        = "CreatedByTerraform"
+    Environment = "Dev"
+    Owner       = "Shubham"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.new_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
